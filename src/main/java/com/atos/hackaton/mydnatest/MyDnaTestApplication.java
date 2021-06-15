458a92d9ad7ddb4307003ec42c2fe1957c50282a
ayoub bahssou <ayoub.bahssou@gmail.com>
2021-06-15 09:14:22 +0000

    Initial commit

---
+++ src/main/java/com/atos/hackaton/mydnatest/MyDnaTestApplication.java
@@ -0,0 +1,13 @@
+package com.atos.hackaton.mydnatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDnaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDnaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atos/hackaton/mydnatest/MyDnaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.hackaton.mydnatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDnaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
