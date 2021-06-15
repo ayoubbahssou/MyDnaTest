2cb4cd823dd5be1071a45c0d8aca747f58a40500
ayoub bahssou <ayoub.bahssou@gmail.com>
2021-06-15 12:08:49 +0000

    add enum and input method

---
+++ src/main/java/com/atos/hackaton/mydnatest/Marqueur.java
@@ -0,0 +1,26 @@
+public enum Marqueur {
+  public final int id;
+
+  Marqueur(int id) {
+    this.id = id;
+  }
+  D3S1358(1),
+  VMA(2),
+  DBS1179(3),
+  D21S11(4),
+  D18S51(5),
+  TH015(6),
+  FGA(7),
+  D16S539(8),
+  D2S1338(9),
+  D19S433(10),
+  Penta-E(11),
+  D5S818(12),
+  D13S317(13),
+  D7S820(14),
+  CSF1PO(15),
+  Penta-d(16),
+  TPOX(17),
+  SE33(18),
+  AMELOGENINE(19);
+}
--- src/main/java/com/atos/hackaton/mydnatest/MyDnaTestApplication.java
+++ src/main/java/com/atos/hackaton/mydnatest/MyDnaTestApplication.java
@@ -8,6 +8,17 @@ public class MyDnaTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyDnaTestApplication.class, args);
+	
+		ArrayList<String> strs = new ArrayList<String>();
+        ArrayList<Double> occurences = new ArrayList<Double>();
+        Scanner sc = new Scanner(System.in);
+
+        for (int t = 0; t < 6; t++) {
+            strs.add(sc.next());
+        }
+        for (int t = 0; t < 6; t++) {
+            occurences.add(sc.nextDouble());
+        }
 	}
 
 }
